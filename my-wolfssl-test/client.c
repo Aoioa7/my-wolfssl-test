1c1eab6f8ef791533e15584937ae97d2346048a9
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-12-18 20:53:53 +0900

    prepare-my-sample-chat

---
+++ my-wolfssl-test/client.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+// wolfSSL関連
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+#include <wolfssl/wolfcrypt/error-crypt.h>
+
+#define PORT 4443
+#define BUF_SIZE 1024
+
+typedef struct {
+    WOLFSSL* ssl;
+} client_info_t;
+
+void* recv_handler(void* arg) {
+    client_info_t* info = (client_info_t*)arg;
+    WOLFSSL* ssl = info->ssl;
+    char buf[BUF_SIZE];
+    int ret;
+    while (1) {
+        memset(buf, 0, BUF_SIZE);
+        ret = wolfSSL_read(ssl, buf, BUF_SIZE - 1);
+        if (ret <= 0) {
+            // サーバー切断 or エラー
+            break;
+        }
+        printf("[RECV] %s\n", buf);
+    }
+    return NULL;
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <server_ip>\n", argv[0]);
+        return 1;
+    }
+
+    const char* server_ip = argv[1];
+
+    int sock;
+    struct sockaddr_in server_addr;
+
+    // wolfSSL初期化
+    wolfSSL_Init();
+    WOLFSSL_CTX* ctx = wolfSSL_CTX_new(wolfTLSv1_2_client_method());
+    if (!ctx) {
+        fprintf(stderr, "wolfSSL_CTX_new error.\n");
+        return 1;
+    }
+
+    // CA証明書でサーバー証明書検証
+    if (wolfSSL_CTX_load_verify_locations(ctx, "ca-cert.pem", NULL) != SSL_SUCCESS) {
+        fprintf(stderr, "Failed to load ca-cert.pem\n");
+        return 1;
+    }
+
+    wolfSSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_port=htons(PORT);
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr)<=0) {
+        perror("inet_pton");
+        return 1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        return 1;
+    }
+
+    WOLFSSL* ssl = wolfSSL_new(ctx);
+    if (!ssl) {
+        fprintf(stderr, "wolfSSL_new error.\n");
+        return 1;
+    }
+
+    wolfSSL_set_fd(ssl, sock);
+
+    if (wolfSSL_connect(ssl) != SSL_SUCCESS) {
+        fprintf(stderr, "wolfSSL_connect failed.\n");
+        wolfSSL_free(ssl);
+        close(sock);
+        return 1;
+    }
+
+    client_info_t info;
+    info.ssl = ssl;
+
+    pthread_t tid;
+    pthread_create(&tid, NULL, recv_handler, &info);
+    pthread_detach(tid);
+
+    // 標準入力からのメッセージ送信ループ
+    char msg[BUF_SIZE];
+    while (1) {
+        if (fgets(msg, BUF_SIZE, stdin) == NULL) {
+            break;
+        }
+        if (strcmp(msg, "/quit\n") == 0) {
+            break;
+        }
+        int ret = wolfSSL_write(ssl, msg, strlen(msg));
+        if (ret <= 0) {
+            break;
+        }
+    }
+
+    wolfSSL_shutdown(ssl);
+    wolfSSL_free(ssl);
+    close(sock);
+    wolfSSL_CTX_free(ctx);
+    wolfSSL_Cleanup();
+
+    return 0;
+}
---
+++ my-wolfssl-test/server.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <errno.h>
+
+// wolfSSL関連
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+#include <wolfssl/wolfcrypt/error-crypt.h>
+
+#define PORT 4443
+#define MAX_CLIENTS 10
+#define BUF_SIZE 1024
+
+typedef struct {
+    WOLFSSL* ssl;
+    int active;
+} client_t;
+
+static WOLFSSL_CTX* ctx = NULL;
+static client_t clients[MAX_CLIENTS];
+static pthread_mutex_t clients_lock = PTHREAD_MUTEX_INITIALIZER;
+
+void broadcast_message(const char* msg, int exclude_index) {
+    pthread_mutex_lock(&clients_lock);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (i != exclude_index && clients[i].active) {
+            int ret = wolfSSL_write(clients[i].ssl, msg, strlen(msg));
+            if (ret <= 0) {
+                // 書き込みエラー時の処理(簡略化)
+            }
+        }
+    }
+    pthread_mutex_unlock(&clients_lock);
+}
+
+void* client_handler(void* arg) {
+    int index = *(int*)arg;
+    free(arg);
+
+    char buf[BUF_SIZE];
+    int ret;
+    WOLFSSL* ssl = clients[index].ssl;
+
+    // クライアントハンドリングループ
+    while (1) {
+        memset(buf, 0, BUF_SIZE);
+        ret = wolfSSL_read(ssl, buf, BUF_SIZE - 1);
+        if (ret <= 0) {
+            // 通信切断またはエラー
+            break;
+        }
+
+        // 受信したメッセージを他クライアントへブロードキャスト
+        broadcast_message(buf, index);
+    }
+
+    // 終了処理
+    pthread_mutex_lock(&clients_lock);
+    wolfSSL_shutdown(ssl);
+    wolfSSL_free(ssl);
+    clients[index].active = 0;
+    pthread_mutex_unlock(&clients_lock);
+
+    return NULL;
+}
+
+
+int main() {
+    int server_fd;
+    struct sockaddr_in server_addr;
+    int opt = 1;
+
+    // wolfSSLライブラリ初期化
+    wolfSSL_Init();
+    ctx = wolfSSL_CTX_new(wolfTLSv1_2_server_method());
+    if (ctx == NULL) {
+        fprintf(stderr, "wolfSSL_CTX_new error.\n");
+        return 1;
+    }
+
+    // サーバー証明書、秘密鍵読み込み
+    if (wolfSSL_CTX_use_certificate_file(ctx, "server-cert.pem", SSL_FILETYPE_PEM) != SSL_SUCCESS) {
+        fprintf(stderr, "Error loading server certificate.\n");
+        return 1;
+    }
+    if (wolfSSL_CTX_use_PrivateKey_file(ctx, "server-key.pem", SSL_FILETYPE_PEM) != SSL_SUCCESS) {
+        fprintf(stderr, "Error loading server key.\n");
+        return 1;
+    }
+
+    // ソケット作成
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        return 1;
+    }
+
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr))<0) {
+        perror("bind failed");
+        return 1;
+    }
+
+    if (listen(server_fd, 5) < 0) {
+        perror("listen failed");
+        return 1;
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        int client_sock = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+        if (client_sock < 0) {
+            perror("accept");
+            continue;
+        }
+
+        // wolfSSL用SSLオブジェクト生成
+        WOLFSSL* ssl = wolfSSL_new(ctx);
+        if (!ssl) {
+            fprintf(stderr, "wolfSSL_new error.\n");
+            close(client_sock);
+            continue;
+        }
+
+        wolfSSL_set_fd(ssl, client_sock);
+
+        if (wolfSSL_accept(ssl) != SSL_SUCCESS) {
+            fprintf(stderr, "wolfSSL_accept failed.\n");
+            wolfSSL_free(ssl);
+            close(client_sock);
+            continue;
+        }
+
+        // クライアントスロットを確保
+        pthread_mutex_lock(&clients_lock);
+        int idx = -1;
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (!clients[i].active) {
+                clients[i].ssl = ssl;
+                clients[i].active = 1;
+                idx = i;
+                break;
+            }
+        }
+        pthread_mutex_unlock(&clients_lock);
+
+        if (idx == -1) {
+            // 定員オーバー
+            wolfSSL_shutdown(ssl);
+            wolfSSL_free(ssl);
+            close(client_sock);
+            continue;
+        }
+
+        int* arg = malloc(sizeof(int));
+        *arg = idx;
+        pthread_t tid;
+        pthread_create(&tid, NULL, client_handler, arg);
+        pthread_detach(tid);
+
+    }
+
+    wolfSSL_CTX_free(ctx);
+    wolfSSL_Cleanup();
+    return 0;
+}
